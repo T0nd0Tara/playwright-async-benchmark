e15f4d4fd02ef8d4959eec9f4652ae0f32afecda
T0nd0Tara <5632815@gmail.com>
2025-08-24 14:40:23 +0300

    add run-all.sh script

---
+++ run-all.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+SERVER_PORT=3000
+TIMEFORMAT=$'\nreal %3lR\nuser %3lU\nsys  %3lS'
+
+tsc &
+TSC_PID=$!
+fuser -k -TERM $SERVER_PORT/tcp
+while (curl http://localhost:$SERVER_PORT >/dev/null 2>&1); do
+  echo "Waiting for port $SERVER_PORT to be close"
+  sleep 1
+done
+npm run server &
+SERVER_PID=$!
+while ! (curl http://localhost:$SERVER_PORT >/dev/null 2>&1); do
+  echo "Waiting for port $SERVER_PORT to be open"
+  sleep 1
+done
+echo "Server is running"
+wait $TSC_PID
+
+# PW workers stuff
+for workers in 1 2 3 4 5 6 7 8 16 32; do
+  npm run pw:$workers
+done
+
+# Close server
+fuser -k -TERM $SERVER_PORT/tcp
