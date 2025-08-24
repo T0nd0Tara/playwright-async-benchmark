3af4a2268db13f348ac9993cea59f20f2cc0d71f
T0nd0Tara <5632815@gmail.com>
2025-08-24 13:52:30 +0300

    add multiplier so I wouldnt need to write so many tests

--- src/all_tests.ts
+++ src/all_tests.ts
@@ -3,12 +3,22 @@ import path from 'path';
 import { globSync } from "fs";
 
 export function getAllTests(): TestArgs[] {
+  const mult = 5;
+
   const pathPrefix = path.join(__dirname, 'tests')
-  const allTests: TestArgs[][] = []
+  const allTests: TestArgs[] = []
 
   for (const entry of globSync(`${pathPrefix}/**/*.{ts,js}`)) {
     const { tests }: { tests: TestArgs[] } = require(entry)
-    allTests.push(tests);
+
+    tests.forEach(singleTest => {
+      for (let i = 0; i < mult; i++) {
+        const testProps: TestArgs = [...singleTest];
+        testProps[0] += `_${i}`
+        allTests.push(testProps);
+      }
+    })
   }
-  return allTests.flat()
+
+  return allTests
 }
