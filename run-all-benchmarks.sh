f08ae94a19eb81c0b3970e95a420ae6f3772a01b
T0nd0Tara <5632815@gmail.com>
2025-08-25 21:29:17 +0300

    rerun all tests after adding new context for each page

--- run-all-benchmarks.sh
+++ run-all-benchmarks.sh
@@ -2,7 +2,8 @@
 set -x
 
 for script in run-all run-best run-fair; do
+  logs_path="./run-logs/$script"
   for i in 0 1 2; do
-    ./$script.sh >|./run-logs/$script/ubuntu-chrome-$i.txt
+    ./$script.sh >|$logs_path/ubuntu-chrome-$i.txt
   done
 done
